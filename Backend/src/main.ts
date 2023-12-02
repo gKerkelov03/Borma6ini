68b272e05a5d652817ba7054e15ef76f3749cacd
Georgi Kerkelov <gkerkelov03@abv.bg>
2023-12-02 14:55:32 +0200

    setup the app module and main.ts

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory, Reflector } from '@nestjs/core';
+import passport from 'passport';
 import { AppModule } from './app.module';
+import { RolesGuard } from './shared/guards/roles.guard';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule),
+        config = app.get(ConfigService),
+        reflector = app.get(Reflector),
+        logger = new Logger('Main');
+
+    app.enableCors({ credentials: true, origin: 'http://localhost:4200' });
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true }))
+        .useGlobalGuards(new RolesGuard(reflector))
+        .use(passport.initialize());
+
+    await app.listen(config.get('PORT') || 3000, () =>
+        logger.log(`listening on port: ${config.get('PORT')}`)
+    );
 }
+
 bootstrap();
